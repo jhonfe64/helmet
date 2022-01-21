4078a8d24d4a5078d4cc31799750583f548fa198
john.perez3 <john.perez@pluriza.com>
2022-01-20 20:56:09 -0500

    add title

--- src/Users.js
+++ src/Users.js
@@ -12,7 +12,7 @@ function Users() {
         <meta property="og:description"        content="descripcion de la pagina users" />
         <meta property="og:image"              content="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80" />
       </Helmet>
-      <h1>esta es la pagina de about</h1>
+      <h1>esta es la pagina de users</h1>
   </div>;
 }
 
