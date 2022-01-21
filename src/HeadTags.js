4f65a506be884b3838cf4ba2379239af2864684f
john.perez3 <john.perez@pluriza.com>
2022-01-21 11:29:02 -0500

    head tag

--- src/HeadTags.js
+++ src/HeadTags.js
@@ -13,16 +13,11 @@ function HeadTags(props) {
   return(
     <Helmet>
       <title>{title}</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="description" key="description" content={metaDescription} />
-      <meta name="title" key="title" content={title} />
-      <meta property="og:title" key="og:title" content={title} />
-      <meta property="og:locale" key="og:locale" content="en_US" />
-      <meta charSet="utf-8" />
-      <meta property="og:type" key="og:type" content="website" />
-      <meta property="og:description"  key="og:description" content={metaDescription}/>
+      <meta property="og:title" content={title} />
+      <meta property="og:type" content="website" />
+      <meta property="og:description" content={metaDescription}/>
       <meta property="og:url" content={url} />
+      <meta property="og:image" content={img}/>  
     </Helmet>
   ) ;
 }
